51c5dc43a0f9785b7ab85e8eccedb87b6d5e4339
Jamie <100721920+Jiblets4Free@users.noreply.github.com>
2022-07-10 12:54:29 +0100

    Managed to succesfully set up an API call.

--- Destiny Tracker.py
+++ Destiny Tracker.py
@@ -1,35 +1,6 @@
-import pydest
-import asyncio
+import requests
 
-
-platforms = {'XBOX': 1, 'PLAYSTATION': 2, 'PC': 3}
-
-async def main():
-    destiny = pydest.Pydest('bc7717e228cd49e58ec26949bba34f51')
-
-    platform = None
-    while not platform:
-        user_input = input('Enter your platform (xbox, playstation, or pc): ')
-        if user_input.upper() in platforms.keys():
-            platform = platforms.get(user_input.upper())
-        else:
-            print('Invalid platform.')
-
-    username = input('Enter the username to locate: ')
-    res = await destiny.api.search_destiny_player(platform, username)
-
-    print(res)
-
-    if res['ErrorCode'] == 1 and len(res['Response']) > 0:
-        print("---")
-        print("Player found!")
-        print("Display Name: {}".format(res['Response'][0]['displayName']))
-        print("Membership ID: {}".format(res['Response'][0]['membershipId']))
-    else:
-        print("Could not locate player.")
-
-    await destiny.close()
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.close()
+api_url = "https://www.bungie.net/Platform/Destiny2/1/Profile/4611686018456716623/?components=205"
+response = requests.get(api_url,headers={"x-api-key":"bc7717e228cd49e58ec26949bba34f51"})
+JSON_response = response.json()
+print(JSON_response)
