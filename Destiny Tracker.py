4bb3249bff8976c3d53bd65ea2ff23edc3b8a008
Jamie <100721920+Jiblets4Free@users.noreply.github.com>
2022-07-10 01:13:18 +0100

    Trying to set up an API key

--- Destiny Tracker.py
+++ Destiny Tracker.py
@@ -1,13 +1,37 @@
-## Destiny Tracker
+## Jib's playDestiny Tracker
 
 import pydest
 import asyncio
 
-async def Main():
-    destiny = pydest.Pydest("APIKEY")
-    json = await destiny.api.search_destiny_player(1, 'Acer, Topper of Laps')
-    print(json)
+
+platforms = {'XBOX': 1, 'PLAYSTATION': 2, 'PC': 3}
+
+async def main():
+    destiny = pydest.Pydest('bc7717e228cd49e58ec26949bba34f51')
+
+    platform = None
+    while not platform:
+        user_input = input('Enter your platform (xbox, playstation, or pc): ')
+        if user_input.upper() in platforms.keys():
+            platform = platforms.get(user_input.upper())
+        else:
+            print('Invalid platform.')
+
+    username = input('Enter the username to locate: ')
+    res = await destiny.api.search_destiny_player(platform, username)
+
+    print(res)
+
+    if res['ErrorCode'] == 1 and len(res['Response']) > 0:
+        print("---")
+        print("Player found!")
+        print("Display Name: {}".format(res['Response'][0]['displayName']))
+        print("Membership ID: {}".format(res['Response'][0]['membershipId']))
+    else:
+        print("Could not locate player.")
+
     await destiny.close()
 
-if __name__ == "__main__":
-    Main()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
+loop.close()
